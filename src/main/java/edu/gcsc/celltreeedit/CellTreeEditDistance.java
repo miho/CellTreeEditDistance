b2c88a7aec858675d0ca0cf3b60aa947587fbd43
Lukas Maurer <lukas.maurer@web.de>
2019-06-10 01:22:22 +0200

    multithreaded included

--- src/main/java/edu/gcsc/celltreeedit/CellTreeEditDistance.java
+++ src/main/java/edu/gcsc/celltreeedit/CellTreeEditDistance.java
@@ -1,15 +1,18 @@
 package edu.gcsc.celltreeedit;
 
-import eu.mihosoft.ext.apted.distance.APTED;
-import eu.mihosoft.ext.apted.node.Node;
-import eu.mihosoft.vrl.annotation.ComponentInfo;
-import eu.mihosoft.vrl.annotation.ParamInfo;
+        import eu.mihosoft.ext.apted.distance.APTED;
+        import eu.mihosoft.ext.apted.node.Node;
+        import eu.mihosoft.vrl.annotation.ComponentInfo;
+        import eu.mihosoft.vrl.annotation.ParamInfo;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
-import java.util.*;
+        import javax.swing.*;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
+        import java.io.*;
+        import java.util.*;
+        import java.util.concurrent.ExecutorService;
+        import java.util.concurrent.Executors;
+        import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Erid on 16.02.2018.
@@ -27,7 +30,6 @@ public class CellTreeEditDistance implements java.io.Serializable{
     private List<String> importedFileNames = new ArrayList<>();
     private List<File> importedFiles = new ArrayList<>();
 
-
     public static void showLabels(){
         JFrame frame = new JFrame();
         Tables labels= new Tables();
@@ -36,7 +38,7 @@ public class CellTreeEditDistance implements java.io.Serializable{
         frame.setSize(500,350);
         frame.setVisible(true);
         frame.setTitle("Labels");
-    //    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
@@ -79,8 +81,41 @@ public class CellTreeEditDistance implements java.io.Serializable{
         }
     }
 
+    static class MyTask implements Runnable {
+
+        private final List<Node<NodeData>> nodeData;
+        private int i;
+        private int j;
+        private float[][] results;
+        private APTED apted;
+        private int choice;
+
+        public MyTask(int i, int j, float[][] results, APTED apted, int choice, List<Node<NodeData>> nodeData) {
+            this.i = i;
+            this.j = j;
+            this.results = results;
+            this.apted = apted;
+            this.choice = choice;
+            this.nodeData = nodeData;
+        }
+
+        @Override
+        public void run() {
+
+            float result = apted.computeEditDistance(nodeData.get(i), nodeData.get(j));
+
+            // TODO sync
+            results[i][j]=result;
+            results[j][i]=result;
+        }
+    }
+
     public void compareFiles(@ParamInfo(name="Label", style="load-folder-dialog")int choice) {
-        int size = files.length;
+
+        long runtimeInS = 0;
+        final long start = System.nanoTime();
+
+        int size= files.length;
         int nrofCalculations= ((size*size)-size)/2;
         int progress=0;
         results= new float[size][size];
@@ -88,34 +123,80 @@ public class CellTreeEditDistance implements java.io.Serializable{
         for(int i=0;i<size;i++){
             names[i]=files[i].getName();
         }
+        final float[][] resultsFinal = results;
+
+        List<Node<NodeData>> nodeData = Collections.synchronizedList(new ArrayList<Node<NodeData>>(size));
+        for(int i = 0; i < size;i++) {
+            nodeData.add(null);
+        }
+
         try {
+
+            System.out.println("> Reading files");
+
+            ExecutorService filePool = Executors.newWorkStealingPool();
+
+
+            for(int i=0; i<size;i++){
+
+                // compare each two files
+
+                final int finalI = i;
+
+                filePool.execute(() -> {
+                    Node<NodeData> t1 = nodeData.get(finalI);
+
+                    if (t1 == null) {
+                        TreeCreator one = null;
+                        try {
+                            one = new TreeCreator(new FileInputStream(files[finalI]));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        t1 = one.createTree(choice, 0);
+                        nodeData.set(finalI, t1);
+                    }
+                });
+            }
+
+            filePool.shutdown();
+
+            while(!filePool.awaitTermination(10L, TimeUnit.SECONDS)) {
+                System.out.println("Still waiting for file readers: " + new Date());
+            }
+
+            ExecutorService pool = Executors.newWorkStealingPool();
+
             System.out.println("* Progress *");
-            for(int i=0; i<size-1;i++){
+            for(int i=0; i<size-1;i++) {
+
                 for(int j=i+1; j<size;j++){
                     // compare each two files
-                    FileInputStream f= new FileInputStream(files[i]);
-                    FileInputStream f2= new FileInputStream(files[j]);
-
-                    TreeCreator one = new TreeCreator(f);
-                    TreeCreator two = new TreeCreator(f2);
-
-                    Node<NodeData> t1 = one.createTree(choice,0);
-                    Node<NodeData> t2 = two.createTree(choice,0);
 
                     // Initialise APTED.
                     APTED<TreeCostModel, NodeData> apted = new APTED<>(new TreeCostModel());
                     // Execute APTED.
-                    float result = apted.computeEditDistance(t1, t2);
-                    results[i][j]=result;
-                    results[j][i]=result;
+
+                    Runnable myTask = new MyTask(i,j,resultsFinal, apted, choice, nodeData);
+                    pool.execute(myTask);
+
                     progress++;
                     System.out.println("* Progress....:"+progress+"/"+nrofCalculations);
                 }
             }
             System.out.println("*");
-        } catch (IOException e) {
+
+
+            pool.shutdown();
+
+            while(!pool.awaitTermination(10L,TimeUnit.SECONDS)) {
+                System.out.println("Still waiting for results: " + new Date());
+            }
+
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
 
         JFrame frame= new JFrame();
         Tables tables = new Tables(fileNames,results);
@@ -133,5 +214,9 @@ public class CellTreeEditDistance implements java.io.Serializable{
         frame.setVisible(true);
         frame.setTitle("Comparison Results");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        final long runtimeInNanos = System.nanoTime() - start;
+        runtimeInS = TimeUnit.NANOSECONDS.toSeconds(runtimeInNanos);
+        System.out.println(runtimeInS);
     }
 }
--- src/main/java/edu/gcsc/celltreeedit/Main.java
+++ src/main/java/edu/gcsc/celltreeedit/Main.java
@@ -5,8 +5,11 @@ import edu.gcsc.celltreeedit.NeuronMetadata.NeuronMetadataRImpl;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 /**
  * Created by Erid on 12.02.2018.
@@ -14,87 +17,65 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//
-////        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-////        String appConfigPath = rootPath + "/main/resources/AppConfig.properties";
-//        String appConfigPath = "src/main/resources/AppConfig.properties";
-//
-//        Properties appProps = new Properties();
-//        appProps.load(new FileInputStream(appConfigPath));
-//
-//        // get Default-Values Application-Properties
-//        File swcDirectory = new File(appProps.getProperty("swcDirectory"));
-//        File metadataPath = new File(appProps.getProperty("metadataPath"));
-//        String exportedMatrixName = appProps.getProperty("exportedMatrixName");
-//        File exportedMatrixPath = new File(appProps.getProperty("exportedMatrixPath"));
-//        int calcType = Integer.parseInt(appProps.getProperty("calcType"));
-//        File jsonDirectory = new File(appProps.getProperty("jsonDirectory"));
-//        boolean show = Boolean.parseBoolean(appProps.getProperty("show"));
-//
-//        for (final String arg: args) {
-//            if (hasValuedParam(arg, AppParameter.CALC_SHORT)){
-//                calcType = extractInt(arg, AppParameter.CALC_SHORT);
-//            }
-//            if (hasValuedParam(arg, AppParameter.CALC)){
-//                calcType = extractInt(arg, AppParameter.CALC);
-//            }
-//            if (hasValuedParam(arg, AppParameter.JSON_SHORT)){
-//                jsonDirectory = new File(extractString(arg, AppParameter.JSON_SHORT));
-//            }
-//            if (hasValuedParam(arg, AppParameter.JSON)){
-//                jsonDirectory = new File(extractString(arg, AppParameter.JSON));
-//            }
-//            if (hasValuedParam(arg, AppParameter.SHOW_SHORT)) {
-//                show = extractBoolean(arg, AppParameter.SHOW_SHORT);
-//            }
-//            if (hasValuedParam(arg, AppParameter.SHOW)) {
-//                show = extractBoolean(arg, AppParameter.SHOW);
+
+//        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+//        String appConfigPath = rootPath + "/main/resources/AppConfig.properties";
+        String appConfigPath = "src/main/resources/AppConfig.properties";
+
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
+
+        // get Default-Values Application-Properties
+        File swcDirectory = new File(appProps.getProperty("swcDirectory"));
+        File metadataPath = new File(appProps.getProperty("metadataPath"));
+        String exportedMatrixName = appProps.getProperty("exportedMatrixName");
+        File exportedMatrixPath = new File(appProps.getProperty("exportedMatrixPath"));
+        int calcType = Integer.parseInt(appProps.getProperty("calcType"));
+        File jsonDirectory = new File(appProps.getProperty("jsonDirectory"));
+        boolean show = Boolean.parseBoolean(appProps.getProperty("show"));
+
+        for (final String arg: args) {
+            if (hasValuedParam(arg, AppParameter.CALC_SHORT)){
+                calcType = extractInt(arg, AppParameter.CALC_SHORT);
+            }
+            if (hasValuedParam(arg, AppParameter.CALC)){
+                calcType = extractInt(arg, AppParameter.CALC);
+            }
+            if (hasValuedParam(arg, AppParameter.JSON_SHORT)){
+                jsonDirectory = new File(extractString(arg, AppParameter.JSON_SHORT));
+            }
+            if (hasValuedParam(arg, AppParameter.JSON)){
+                jsonDirectory = new File(extractString(arg, AppParameter.JSON));
+            }
+            if (hasValuedParam(arg, AppParameter.SHOW_SHORT)) {
+                show = extractBoolean(arg, AppParameter.SHOW_SHORT);
+            }
+            if (hasValuedParam(arg, AppParameter.SHOW)) {
+                show = extractBoolean(arg, AppParameter.SHOW);
+            }
+        }
+
+
+        CellTreeEditDistance matrix = new CellTreeEditDistance();
+        if (!jsonDirectory.getName().equals("")) {
+            Set<String> swcFileNames = Utils.parseJsonToFileNames(jsonDirectory);
+            matrix.compareFilesFromFilenames(swcFileNames, swcDirectory, 9);
+        } else {
+            matrix.compareFilesFromChoose(9);
+        }
+
+
+
+//        SWCTest swcTest= new SWCTest();
+//        Set<String> fileNames = swcTest.preprocessSWCDirectory(neuronMetadata, swcDirectory);
+
+//        for (String fileName: fileNames) {
+//            if (!neuronMetadata.containsKey(fileName)) {
+//                System.out.println("File nicht in Metadaten: " + fileName);
 //            }
 //        }
 
-        File metadataPath = new File("/media/exdisk/Sem06/BA/Neuromorpho/Metadaten");
-        NeuronMetadataMapper mapper = new NeuronMetadataMapper();
-        Map<String, NeuronMetadataRImpl> neuronMetadataRImpl = mapper.mapFromDirectory(metadataPath);
-        Float somaSurface;
-        for (String neuronName : neuronMetadataRImpl.keySet()) {
-//            System.out.println(neuronName);
-            somaSurface = neuronMetadataRImpl.get(neuronName).getCorrectedXy();
-            if (somaSurface != null) {
-                System.out.println(somaSurface.toString());
-            }
-        }
-        System.out.println(neuronMetadataRImpl.size());
 
-//
-////        CellTreeEditDistance matrix = new CellTreeEditDistance();
-////        if (!jsonDirectory.getName().equals("")) {
-////            Set<String> swcFileNames = Utils.parseJsonToFileNames(jsonDirectory);
-////            matrix.compareFilesFromFilenames(swcFileNames, swcDirectory, 9);
-////        } else {
-////            matrix.compareFilesFromChoose(9);
-////        }
-//
-//
-//
-//        SWCTest swcTest= new SWCTest();
-//        Set<String> fileNames = swcTest.preprocessSWCDirectory(neuronMetadata, swcDirectory);
-//
-////        for (String fileName: fileNames) {
-////            if (!neuronMetadata.containsKey(fileName)) {
-////                System.out.println("File nicht in Metadaten: " + fileName);
-////            }
-////        }
-//
-////        File[] files = Utils.choose();
-////        String filename;
-////        for (File file: files) {
-////            filename = file.getName();
-////            filename = filename.substring(0, filename.substring(0, filename.lastIndexOf(".")).lastIndexOf("."));
-////            System.out.println(neuronMetadata.get(filename).neuronName);
-////        }
-//
-////         CellTreeEditDistance.showLabels();
-////         System.out.print(Main.class.getClassLoader().getResourceAsStream("/src/main/resources/TestSWCFiles/1k.swc"));
 
     }
 
