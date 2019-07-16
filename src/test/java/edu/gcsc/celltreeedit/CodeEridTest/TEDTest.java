5ede73a13d3cc34d3daf516aeeed8019e961da45
Lukas Maurer <lukas.maurer@web.de>
2019-07-16 13:03:55 +0200

    checkTEDSameTree1NodeRenamed works

--- src/test/java/edu/gcsc/celltreeedit/CodeEridTest/TEDTest.java
+++ src/test/java/edu/gcsc/celltreeedit/CodeEridTest/TEDTest.java
@@ -56,8 +56,6 @@ public class TEDTest {
                 expectedResult += node.getNodeData().getLabel();
             }
             float expectedResultFloat = (float) expectedResult;
-            System.out.println("expectedResult: " + expectedResult);
-            System.out.println("actualResult: " + result);
             assertEquals(expectedResultFloat, result, deltaBySize(expectedResult));
 
 
@@ -69,139 +67,54 @@ public class TEDTest {
                 expectedResult += node.getNodeData().getLabel();
             }
             expectedResultFloat = (float) expectedResult;
-            System.out.println("expectedResult: " + expectedResult);
-            System.out.println("actualResult: " + result);
             assertEquals(expectedResultFloat, result, deltaBySize(expectedResult));
         }
     }
 
-
     @Test
-    public void checkTEDSameTreesLeafInserted() throws IOException {
+    public void checkTEDSuperSimpleTreesCreatedManually() {
 
-        double lengthOfSegment = 1;
-        double volumeOfSegment = Math.PI * 1 * 1 * lengthOfSegment;
-        double surfaceOfSegment = 2 * Math.PI * 1 * lengthOfSegment;
+        double labelValue1 = 1d/3;
+        double labelValue2 = 1d/2;
 
-//        double lengthOfT = 20.6838908114423 + lengthOfSegment;
-//        double volumeOfT = 195945673.142389 + volumeOfSegment;
-//        double surfaceOfT = 741241459.754624 + surfaceOfSegment;
+        // create first tree manually
+        NodeData nodeData = new NodeData(1);
+        Node<NodeData> root1 = new Node<>(nodeData);
+        nodeData = new NodeData(1);
+        Node<NodeData> leftChild1 = new Node<>(nodeData);
+        root1.addChild(leftChild1);
+        nodeData = new NodeData(1);
+        Node<NodeData> rightChild1 = new Node<>(nodeData);
+        root1.addChild(rightChild1);
+        // set labels manually
+        root1.getNodeData().setLabel(labelValue1);
+        leftChild1.getNodeData().setLabel(labelValue1);
+        rightChild1.getNodeData().setLabel(labelValue1);
 
-        double lengthOfT = 2 * lengthOfSegment;
-        double volumeOfT = 2 * volumeOfSegment;
-        double surfaceOfT = 2 * surfaceOfSegment;
+        // create second tree manually
+        nodeData = new NodeData(1);
+        Node<NodeData> root2 = new Node<>(nodeData);
+        nodeData = new NodeData(1);
+        Node<NodeData> leftChild2 = new Node<>(nodeData);
+        root2.addChild(leftChild2);
 
-        float result;
-        double testTED;
+        // set labels manually
+        root2.getNodeData().setLabel(labelValue2);
+        leftChild2.getNodeData().setLabel(labelValue2);
 
-        for (int i = 1; i < 23; i++) {
-            System.out.println(i);
-            FileInputStream f1 = new FileInputStream(new File("/media/exdisk/Sem06/BA/ProgramData/Data/Test/TEDTest01_superSimpleTree.swc"));
-            TreeCreator t1 = new TreeCreator(f1);
-            Node<NodeData> root1 = t1.createTree(i);
 
-            FileInputStream f2 = new FileInputStream(new File("/media/exdisk/Sem06/BA/ProgramData/Data/Test/TEDTest01_superSimpleTree_1NodeInserted.swc"));
-            TreeCreator t2 = new TreeCreator(f2);
-            Node<NodeData> root2 = t2.createTree(i);
+        APTED<TreeCostModel, NodeData> apted = new APTED<>(new TreeCostModel());
+        double result = apted.computeEditDistance(root2, root1);
 
-            APTED<TreeCostModel, NodeData> apted = new APTED<>(new TreeCostModel());
-            result = apted.computeEditDistance(root1, root2);
-
-            testTED = 0;
-            switch (i) {
-                case 1:
-                    testTED = 1d;
-                    break;
-                case 2:
-                    testTED = 1d / 4d;
-                    break;
-                case 3:
-                    testTED = lengthOfSegment;
-                    break;
-                case 4:
-                    testTED = lengthOfSegment;
-                    break;
-                case 5:
-                    testTED = lengthOfSegment;
-                    break;
-                case 6:
-                    testTED = lengthOfSegment / lengthOfT;
-                    break;
-                case 7:
-                    testTED = lengthOfSegment / lengthOfT;
-                    break;
-                case 8:
-                    testTED = lengthOfSegment / lengthOfT;
-                    break;
-                case 9:
-                    testTED = volumeOfSegment;
-                    break;
-                case 10:
-                    testTED = volumeOfSegment;
-                    break;
-                case 11:
-                    testTED = volumeOfSegment;
-                    break;
-                case 12:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
-                case 13:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
-                case 14:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
-                case 15:
-                    testTED = surfaceOfSegment;
-                    break;
-                case 16:
-                    testTED = surfaceOfSegment;
-                    break;
-                case 17:
-                    testTED = surfaceOfSegment;
-                    break;
-                case 18:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 19:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 20:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 21:
-                    testTED = volumeOfSegment / surfaceOfT;
-                    break;
-                case 22:
-                    testTED = 0d;
-                    break;
-                }
-            assertEquals((float) testTED, result, 0f);
-        }
-    }
-
-    private int calculateNoOfNodes(Node<NodeData> node) {
-        int noOfNodes = 1;
-        for (Node<NodeData> child : node.getChildren()) {
-            noOfNodes += calculateNoOfNodes(child);
-        }
-        return noOfNodes;
+        assertEquals(labelValue1, result, 0d);
     }
 
     @Test
-    public void checkTEDSameTreesLeafRenamed() throws IOException {
+    public void checkTEDSameTrees1lengthChanged() throws IOException {
 
-        double lengthOfSegment = 1;
-        double volumeOfSegment = Math.PI * 1 * 1 * lengthOfSegment;
-        double surfaceOfSegment = 2 * Math.PI * 1 * lengthOfSegment;
-
-//        double lengthOfT = 20.6838908114423 + lengthOfSegment;
-//        double volumeOfT = 195945673.142389 + volumeOfSegment;
-//        double surfaceOfT = 741241459.754624 + surfaceOfSegment;
-
-        double lengthOfT = 3 * lengthOfSegment;
-        double volumeOfT = 3 * volumeOfSegment;
-        double surfaceOfT = 3 * surfaceOfSegment;
+        double diffLengthOfSegment = 1;
+        double diffVolumeOfSegment = Math.PI * 1 * 1 * diffLengthOfSegment;
+        double diffSurfaceOfSegment = 2 * Math.PI * 1 * diffLengthOfSegment;
 
         float result;
         double testTED;
@@ -228,22 +141,72 @@ public class TEDTest {
                     testTED = 0d;
                     break;
                 case 3:
+                    testTED = diffLengthOfSegment;
+                    break;
+                case 4:
+                    testTED = diffLengthOfSegment;
+                    break;
+//                case 5:
+//                    testTED
+                case 9:
+                    testTED = diffVolumeOfSegment;
+                    break;
+                case 10:
+                    testTED = diffVolumeOfSegment;
+                    break;
+                case 15:
+                    testTED = diffSurfaceOfSegment;
+                    break;
+                case 16:
+                    testTED = diffSurfaceOfSegment;
+                    break;
+                case 22:
+                    testTED = 0d;
+                    break;
+            }
+            if (i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 10 || i == 15 || i == 16 || i == 22) {
+                assertEquals((float) testTED, result, 0f);
+            }
+        }
+    }
+
+    @Test
+    public void checkTEDSameTreesLeafInserted() throws IOException {
+
+        double lengthOfSegment = 1;
+        double volumeOfSegment = Math.PI * 1 * 1 * lengthOfSegment;
+        double surfaceOfSegment = 2 * Math.PI * 1 * lengthOfSegment;
+
+        float result;
+        double testTED;
+
+        for (int i = 1; i < 23; i++) {
+            System.out.println(i);
+            FileInputStream f1 = new FileInputStream(new File("/media/exdisk/Sem06/BA/ProgramData/Data/Test/TEDTest01_superSimpleTree.swc"));
+            TreeCreator t1 = new TreeCreator(f1);
+            Node<NodeData> root1 = t1.createTree(i);
+
+            FileInputStream f2 = new FileInputStream(new File("/media/exdisk/Sem06/BA/ProgramData/Data/Test/TEDTest01_superSimpleTree_1NodeInserted.swc"));
+            TreeCreator t2 = new TreeCreator(f2);
+            Node<NodeData> root2 = t2.createTree(i);
+
+            APTED<TreeCostModel, NodeData> apted = new APTED<>(new TreeCostModel());
+            result = apted.computeEditDistance(root1, root2);
+
+            testTED = 0;
+            switch (i) {
+                case 1:
+                    testTED = 1d;
+                    break;
+                case 2:
+                    // all nodes have to be renamed and one node with label 1/4 has to be inserted
+                    testTED = 1d/4 + 3* (1d/3 - 1d/4);
+                    break;
+                case 3:
                     testTED = lengthOfSegment;
                     break;
                 case 4:
                     testTED = lengthOfSegment;
-                    break;
-                case 5:
-                    testTED = lengthOfSegment;
-                    break;
-                case 6:
-                    testTED = lengthOfSegment / lengthOfT;
-                    break;
-                case 7:
-                    testTED = lengthOfSegment / lengthOfT;
-                    break;
-                case 8:
-                    testTED = lengthOfSegment / lengthOfT;
                     break;
                 case 9:
                     testTED = volumeOfSegment;
@@ -251,44 +214,19 @@ public class TEDTest {
                 case 10:
                     testTED = volumeOfSegment;
                     break;
-                case 11:
-                    testTED = volumeOfSegment;
-                    break;
-                case 12:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
-                case 13:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
-                case 14:
-                    testTED = volumeOfSegment / volumeOfT;
-                    break;
                 case 15:
                     testTED = surfaceOfSegment;
                     break;
                 case 16:
                     testTED = surfaceOfSegment;
                     break;
-                case 17:
-                    testTED = surfaceOfSegment;
-                    break;
-                case 18:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 19:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 20:
-                    testTED = surfaceOfSegment / surfaceOfT;
-                    break;
-                case 21:
-                    testTED = volumeOfSegment / surfaceOfT;
-                    break;
                 case 22:
                     testTED = 0d;
                     break;
             }
-            assertEquals((float) testTED, result, 0f);
+            if (i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 10 || i == 15 || i == 16 || i == 22) {
+                assertEquals((float) testTED, result, 0f);
+            }
         }
     }
 }
