db3ad72411a6e7b4bb6d9050189baba8bfdb679b
Lukas Maurer <lukas.maurer@web.de>
2019-07-17 02:18:18 +0200

    ClusteringTest-File added

--- src/test/java/edu/gcsc/celltreeedit/CodeEridTest/ClusteringTest.java
+++ src/test/java/edu/gcsc/celltreeedit/CodeEridTest/ClusteringTest.java
@@ -1,4 +1,7 @@
 package edu.gcsc.celltreeedit.CodeEridTest;
 
 public class ClusteringTest {
+    // topology of clustering correct?
+    // calculated distance correct?
+    // limitCluster works correctly?
 }
