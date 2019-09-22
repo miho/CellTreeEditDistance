1eb07d64013b99801c1319ee4089647b3192c85f
Lukas Maurer <lukas.maurer@web.de>
2019-09-22 23:15:35 +0200

    rEnameDendrogram

--- src/main/java/edu/gcsc/celltreeedit/AppProperties/AppParameter.java
+++ src/main/java/edu/gcsc/celltreeedit/AppProperties/AppParameter.java
@@ -10,7 +10,7 @@ enum AppParameter {
     FILE_INPUT("f", "fileInput", true, "defines the file used for calculation (can be a json-file containing the swc-files used for TED-calculation or a txt-file containing the distance-matrix for dendrogram-calculation."),
     NAME_OUTPUT("n", "nameOutput", true, "defines the basic name used for naming the output. appendices might be added. no fileextension needed."),
     LABEL("l", "label", true, "defines the label used for TED-Calculation."),
-    RENAME_DENDROGRAM("r", "renameDendrogram", false, "if used, filenames in dendrogram are replaced by a combination of neuronMetadata."),
+    RENAME_DENDROGRAM("e", "rEnameDendrogram", false, "if used, filenames in dendrogram are replaced by a combination of neuronMetadata."),
     SAVE_OUTPUT("s", "saveOutput", false, "if used, results are saved to output-directory (might include distance-matrix, dendrogram, filemapping)."),
     ROWS("r", "rowsPerCall", true, "used for TED-Calculation on a Cluster. Defines the number of matrix-rows for which TED is calculated per call. To guarantee division in same problem sizes only about half of the columns are calculated."),
     ITERATION("i", "iteration", true, "used for TED-Calculation on a Cluster. Defines the actual iteration of a TED-Calculation divided for multiple cluster-nodes.");
